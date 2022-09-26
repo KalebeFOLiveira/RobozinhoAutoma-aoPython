118ed299b0674057394cc7337c959622f8a9bd92
AdersonSilva18 <75860265+AdersonSilva18@users.noreply.github.com>
2022-09-26 12:31:05 -0300

    Ajustes gerais

--- GoogleApi.py
+++ GoogleApi.py
@@ -1,19 +1,25 @@
-import email
-from Google import Create_Service
 import json
+from Google import Create_Service
 
 ARQUIVO_CLIENTE = 'credentials.json'
 API_NOME = 'gmail'
 VERSAO_DA_API = 'v1'
 ESCOPOS = ['https://mail.google.com/']
 service = Create_Service(ARQUIVO_CLIENTE,API_NOME,VERSAO_DA_API,ESCOPOS)
-# user_id = 
-print(service.users().getProfile(userId='me').execute())
-print(service.users().messages().list(userId='me').execute())
-message = service.users().messages().get(userId='me',id ='18337b9e8d734848').execute()
-s1 = json.dumps(message)
-d2 = json.loads(s1)
-print(d2)
-list (email)
-while{message.titulo!=titulocodigo?}
-list money=nova lista
+
+emails = service.users().messages().list(userId='me').execute()
+
+# print(service.users().getProfile(userId='me').execute())
+
+print(emails['messages'][0]['id'])
+
+# print(service.users().messages().get(userId='me',id = emails['messages'][11]['id']).execute()['snippet'])
+
+while(emails['messages'][0]['id'] != '0999'):
+    print(emails['messages'][0]['id'])
+    print(service.users().messages().get(userId='me',id = emails['messages'][0]['id']).execute()['snippet'])
+    emails = service.users().messages().list(userId='me').execute()
+# for item in emails['messages']:
+#     print(service.users().messages().get(userId='me',id = item['id']).execute()['id'])
+#     print(service.users().messages().get(userId='me',id = item['id']).execute()['snippet'])
+    
